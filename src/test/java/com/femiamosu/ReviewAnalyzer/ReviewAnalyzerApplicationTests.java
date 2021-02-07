c2f2f07470a904bc8bf7f64cf1aac71ee8a3168c
student <nicefemi@yahoo.com>
2021-02-07 14:46:00 +0000

    Add logic and test

---
+++ src/test/java/com/femiamosu/ReviewAnalyzer/ReviewAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femiamosu.ReviewAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
